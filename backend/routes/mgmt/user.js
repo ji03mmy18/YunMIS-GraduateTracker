42d38c8396673b2977434a8ad022fe97069deafd
YoMin Su <aaaa1234567891016@gmail.com>
2023-02-06 23:55:48 +0800

    feat: [後端]完成管理員管理與權限設定功能
    系統可以由初次建立時新增的進階管理員對系統進行普通管理員的新增、修改、刪除，方便資料的維護。

--- backend/routes/mgmt/user.js
+++ backend/routes/mgmt/user.js
@@ -1,9 +1,125 @@
 let express = require('express');
 let router = express.Router({mergeParams: true});
 
+let { pwHash } = require('../../utils/tool');
+
+// 取得管理員列表
 router.get('/list', async (req, res, next) => {
-  res.status(200).json({ status: true, msg: "OK!" });
-  return;
-})
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    const rows = await conn.query("SELECT User, Nick, Deletable, PermCreate, PermRead, PermUpdate, PermDelete FROM manager");
+    if (rows[0] === undefined)  {
+      res.status(404).json({ status: false, msg: 'ManagerNotFound' });
+      return;
+    }
+
+    // 回傳查詢結果
+    res.status(200).json({ status: true, data: rows });
+    return;
+  } finally {
+    conn.end();
+  }
+});
+
+// 新增管理員
+router.post('/new', async (req, res, next) => {
+  let {
+    user, pass, nick, pCreate,
+    pRead, pUpdate, pDelete
+  } = req.body;
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    // 新增資料到資料庫中
+    const result = await conn.query(
+      "INSERT INTO manager(User, Passwd, Nick, Deletable, PermCreate, PermRead, PermUpdate, PermDelete) \
+      VALUES (?, ?, ?, '1', ?, ?, ?, ?)", [user, pwHash(pass), nick, pCreate, pRead, pUpdate, pDelete]);
+    if (result.affectedRows != 1) {
+      res.status(500).json({ status: false, msg: 'ManagerAddFailed'});
+      return;
+    }
+
+    // 回傳結果
+    res.status(200).json({ status: true, msg: 'Done' });
+    return;
+  } finally {
+    conn.end();
+  }
+});
+
+// 修改管理員設定
+router.patch('/:id', async (req, res, next) => {
+  let uid = req.params.id;
+  let {
+    user, pass, nick, pCreate,
+    pRead, pUpdate, pDelete
+  } = req.body;
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    // 請求檢查
+    if (uid != user) {
+      res.status(400).json({ status: false, msg: 'AccountMismatch' });
+      return;
+    }
+
+    // 操作資料庫進行更新
+    const result = await conn.query("UPDATE manager SET Passwd = ?, Nick = ?, \
+      PermCreate = ?, PermRead = ?, PermUpdate = ?, PermDelete = ? WHERE User = ?",
+      [pwHash(pass), nick, pCreate, pRead, pUpdate, pDelete, user]
+    );
+    if (result.affectedRows != 1) {
+      res.status(500).json({ status: false, msg: 'ManagerModifyFailed' });
+      return;
+    }
+
+    // 回傳結果
+    res.status(200).json({ status: true, msg: 'Done' });
+    return;
+  } finally {
+    conn.end();
+  }
+});
+
+// 刪除管理員
+router.delete('/:id', async (req, res, next) => {
+  let uid = req.params.id;
+  let user = req.body.user;
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    // 請求檢查
+    if (uid != user) {
+      res.status(400).json({ status: false, msg: 'AccountMismatch' });
+      return;
+    }
+
+    // 查詢管理員是否存在
+    const rows = await conn.query("SELECT * FROM manager WHERE User = ?", [user]);
+    if (rows[0] === undefined) {
+      res.status(404).json({ status: false, msg: 'ManagerNotFound' });
+      return;
+    }
+    // 確認是否可以刪除
+    if (rows[0].Deletable == 0) {
+      res.status(200).json({ status: false, msg: 'ManagerCannotRemove' });
+      return;
+    }
+
+    // 實際刪除管理員
+    const result = await conn.query("DELETE FROM manager WHERE User = ?", [user]);
+    if (result.affectedRows != 1) {
+      res.status(500).json({ status: false, msg: 'ManagerDeleteFailed' });
+      return;
+    }
+
+    // 回傳結果
+    res.status(200).json({ status: true, msg: 'Done' });
+    return;
+  } finally {
+    conn.end();
+  }
+});
 
 module.exports = router;

9033253aca3ed75112928084a14b2f168e9d201a
YoMin Su <aaaa1234567891016@gmail.com>
2023-02-09 09:38:13 +0800

    feat: [前端]新增前端路由與跨頁資料存取套件
    透過 vue-router 與 pinia，使前端可以設計出類似後端的路由系統，同時，pinia可作為全域資料管理工具，儲存使用者的相關資訊。

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import router from '@/router';
+import pinia from '@/store';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import 'vuetify/styles';
+// import { createVuetify } from 'vuetify';
+// import * as components from 'vuetify/components';
+// import * as directives from 'vuetify/directives';
+
+const app = createApp(App);
+// const vuetify = createVuetify({
+//   components,
+//   directives
+// });
+
+app.use(router);
+app.use(pinia);
+app.mount('#app');
---
+++ frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const title = ' - 雲科大資管系 畢業生流向調查系統';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/components/HelloWorld.vue'),
+    meta: { title: titleFormat('Home') }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.title) document.title = to.meta.title;
+  next();
+})
+
+export default router;
+
+function titleFormat(str) {
+  return str + title;
+}

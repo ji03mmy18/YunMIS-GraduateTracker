cc6020004d316f52f366520bcce2e13400e1a8ef
YoMin Su <aaaa1234567891016@gmail.com>
2023-05-26 23:05:18 +0800

    chore: [前端]新增程式中所需的icon

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -2,8 +2,8 @@ import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 import {
   mdiLogout, mdiGithub, mdiGmail, mdiArchive, mdiPencil, mdiBoomGate,
-  mdiDatabaseEditOutline, mdiDatabaseImport,
-  mdiAccountEditOutline, mdiAccountSearch, mdiFilter, mdiMagnify,
+  mdiDatabaseEditOutline, mdiDatabaseImport, mdiAccountEditOutline,
+  mdiAccountSearch, mdiFilter, mdiMagnify, mdiEye, mdiEyeOff,
 } from '@mdi/js';
 import { VDataTable } from 'vuetify/labs/VDataTable';
 import * as components from 'vuetify/components';
@@ -16,6 +16,8 @@ const vuetify = createVuetify({
       ...aliases,
       "mdi-archive": mdiArchive,
       "mdi-boomGate": mdiBoomGate,
+      "mdi-eye": mdiEye,
+      "mdi-eye-off": mdiEyeOff,
       "mdi-filter": mdiFilter,
       "mdi-github": mdiGithub,
       "mdi-gmail": mdiGmail,

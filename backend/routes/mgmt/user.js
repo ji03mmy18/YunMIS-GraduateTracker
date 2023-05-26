e6000373f3529ce9eaf55fdb90d49bef6c8aac4f
YoMin Su <aaaa1234567891016@gmail.com>
2023-05-26 22:30:11 +0800

    feat: [後端]新增超級管理員路由
    提供額外用於新增超級管理員的路由，僅需填寫帳號、密碼、顯示名稱即可。
    切分管理員修改路由，分成基本資料修改與權限修改兩部分。

--- backend/routes/mgmt/user.js
+++ backend/routes/mgmt/user.js
@@ -10,7 +10,10 @@ router.get('/list', async (req, res, next) => {
   let conn = await req.dbPool.getConnection();
 
   try {
-    const rows = await conn.query("SELECT User, Nick, Deletable, PermCreate, PermRead, PermUpdate, PermDelete FROM manager");
+    const rows = await conn.query(
+      "SELECT User, Nick, Deletable, PermCreate, PermRead, PermUpdate, PermDelete\
+      FROM manager ORDER BY Deletable, User"
+    );
     if (rows[0] === undefined)  {
       res.status(404).json({ status: false, msg: 'ManagerNotFound' });
       return;
@@ -57,12 +60,64 @@ router.post('/new', permFieldCheck(), async (req, res, next) => {
   }
 });
 
-// 修改管理員設定
-router.patch('/:id', permFieldCheck(), async (req, res, next) => {
+// 新增超級管理員
+router.post('/snew', async (req, res, next) => {
+  let { user, pass, nick } = req.body;
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    // 新增資料到資料庫中
+    const result = await conn.query(
+      "INSERT INTO manager(User, Passwd, Nick, Deletable, PermCreate, PermRead, PermUpdate, PermDelete) \
+      VALUES (?, ?, ?, '0', '1', '1', '1', '1')", [user, pwHash(pass), nick]);
+    if (result.affectedRows != 1) {
+      res.status(500).json({ status: false, msg: 'ManagerAddFailed'});
+      return;
+    }
+
+    // 回傳結果
+    res.status(200).json({ status: true, msg: 'Done' });
+    return;
+  } finally {
+    conn.end();
+  }
+});
+
+// 修改管理員資訊
+router.patch('/:id', async (req, res, next) => {
+  let uid = req.params.id;
+  let { user, pass, nick } = req.body;
+  let conn = await req.dbPool.getConnection();
+
+  try {
+    // 請求檢查
+    if (uid != user) {
+      res.status(400).json({ status: false, msg: 'AccountMismatch' });
+      return;
+    }
+
+    // 操作資料庫進行更新
+    const result = await conn.query("UPDATE manager SET Passwd = IFNULL(?, Passwd), Nick = ? WHERE User = ?",
+      [pass == '' ? null : pwHash(pass), nick, user]
+    );
+    if (result.affectedRows != 1) {
+      res.status(500).json({ status: false, msg: 'ManagerModifyFailed' });
+      return;
+    }
+
+    // 回傳結果
+    res.status(200).json({ status: true, msg: 'Done' });
+    return;
+  } finally {
+    conn.end();
+  }
+});
+
+// 修改管理員權限
+router.patch('/:id/perm', permFieldCheck(), async (req, res, next) => {
   let uid = req.params.id;
   let {
-    user, pass, nick, pCreate,
-    pRead, pUpdate, pDelete
+    user, pCreate, pRead, pUpdate, pDelete
   } = req.body;
   let conn = await req.dbPool.getConnection();
   let error = validationResult(req);
@@ -81,9 +136,9 @@ router.patch('/:id', permFieldCheck(), async (req, res, next) => {
     }
 
     // 操作資料庫進行更新
-    const result = await conn.query("UPDATE manager SET Passwd = ?, Nick = ?, \
+    const result = await conn.query("UPDATE manager SET \
       PermCreate = ?, PermRead = ?, PermUpdate = ?, PermDelete = ? WHERE User = ?",
-      [pwHash(pass), nick, pCreate, pRead, pUpdate, pDelete, user]
+      [pCreate, pRead, pUpdate, pDelete, user]
     );
     if (result.affectedRows != 1) {
       res.status(500).json({ status: false, msg: 'ManagerModifyFailed' });
@@ -101,18 +156,11 @@ router.patch('/:id', permFieldCheck(), async (req, res, next) => {
 // 刪除管理員
 router.delete('/:id', async (req, res, next) => {
   let uid = req.params.id;
-  let user = req.body.user;
   let conn = await req.dbPool.getConnection();
 
   try {
-    // 請求檢查
-    if (uid != user) {
-      res.status(400).json({ status: false, msg: 'AccountMismatch' });
-      return;
-    }
-
     // 查詢管理員是否存在
-    const rows = await conn.query("SELECT * FROM manager WHERE User = ?", [user]);
+    const rows = await conn.query("SELECT * FROM manager WHERE User = ?", [uid]);
     if (rows[0] === undefined) {
       res.status(404).json({ status: false, msg: 'ManagerNotFound' });
       return;
@@ -124,7 +172,7 @@ router.delete('/:id', async (req, res, next) => {
     }
 
     // 實際刪除管理員
-    const result = await conn.query("DELETE FROM manager WHERE User = ?", [user]);
+    const result = await conn.query("DELETE FROM manager WHERE User = ?", [uid]);
     if (result.affectedRows != 1) {
       res.status(500).json({ status: false, msg: 'ManagerDeleteFailed' });
       return;
